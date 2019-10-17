70c848a72704acc2e758906ef058d19ca593ce5b
Srijith Bharadwaj <srijithdamerla2001@gmail.com>
2019-10-17 09:11:40 +0000

    msm8974: Filter out sm8150 hals
    Signed-off-by: Srijith Bharadwaj <srijithdamerla2001@gmail.com>

--- Android.mk
+++ Android.mk
@@ -9,7 +9,7 @@ endif
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
 else
-ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 sm8150,$(TARGET_BOARD_PLATFORM)),)
     #This is for mako since it doesn't have the QCOM make functions
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
